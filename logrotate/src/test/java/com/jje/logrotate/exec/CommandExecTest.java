08b426996040e819b67df716c94203a40af2011e
xuhaixiang <sean.xu@jinjiang.com>
2012-04-18 17:34:11 +0800

    add exec/cli test

---
+++ logrotate/src/test/java/com/jje/logrotate/exec/CommandExecTest.java
@@ -0,0 +1,33 @@
+package com.jje.logrotate.exec;
+
+import static org.junit.Assert.fail;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteWatchdog;
+import org.apache.commons.exec.Executor;
+import org.apache.commons.exec.OS;
+import org.junit.Test;
+
+public class CommandExecTest {
+
+    @Test
+    public void execls() {
+        ExecuteWatchdog watchdog = new ExecuteWatchdog(30000);
+        Executor exec = new DefaultExecutor();
+        exec.setWatchdog(watchdog);
+        String line = "ls -l";
+        if (OS.isFamilyWindows()) {
+            line = "cmd.exe /C dir";
+        }
+        CommandLine cl = CommandLine.parse(line);
+        try {
+            int exitvalue = exec.execute(cl);
+            if (exec.isFailure(exitvalue) && watchdog.killedProcess()) {
+                fail();
+            }
+        } catch (Exception e) {
+            fail();
+        }
+    }
+}
