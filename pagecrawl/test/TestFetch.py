f92bbbbbb27228f34e88a109b0e4f1e4afb879fc
xuhaixiang <sean.xu@jinjiang.com>
2012-06-25 17:06:31 +0800

    add save load testcase

---
+++ pagecrawl/test/TestFetch.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2.7
+# -*- coding:utf-8 -*-
+
+import unittest
+from core.fetch import INNS
+from core.fetch import JJE
+
+class TestFetch(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.cityDicts = {'1100':'北京', '1200':'天津', '3100':'上海', '4201':'武汉', '5000':'重庆', '6101':'西安', '4101':'郑州', '3201':'南京', '3202':'无锡', '4401':'广州', '5301':'昆明', '4403':'深圳', '4602':'三亚', '3205':'苏州', '3301':'杭州', '4301':'长沙'}
+    
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+    
+    def test_inns_fetch(self):
+        inns = INNS('1100')
+        print inns.fetchPage(inns.makeUrl(1))
+    
+    def test_jje_fetch(self):
+        jje = JJE('北京')
+        print jje.fetchPage(jje.makeUrl(1))
+
+if __name__=='__main__':
+    unittest.main()
---
+++ pagecrawl/test/TestSaveLoad.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2.7
+# -*- coding:utf-8 -*-
+
+import unittest
+import core.crawl as cc
+import os
+import shutil
+import json
+
+class TestSaveLoad(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.root = os.path.dirname(__file__)
+        self.tmpFolderName = 'vardata'
+        self.fileName = 'test.json'
+    
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+        shutil.rmtree(os.sep.join([self.root, self.tmpFolderName]))
+    
+    def test_save_empty(self):
+        cc.save([], self.fileName, self.root, self.tmpFolderName)
+        with open(os.sep.join([self.root, self.tmpFolderName, self.fileName]), 'r') as f:
+            self.assertEqual(0, len(json.load(f)))
+            
+    def test_save_simple(self):
+        cc.save([1,2], self.fileName, self.root, self.tmpFolderName)
+        with open(os.sep.join([self.root, self.tmpFolderName, self.fileName]), 'r') as f:
+            self.assertEqual(2, len(json.load(f)))
+
+
+if __name__=='__main__':
+    unittest.main()
